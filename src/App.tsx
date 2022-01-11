32996c102a38d013dac76371c7db56b044d6e57b
Dasha1801 <6227968@gmail.com>
2022-01-11 10:55:38 +0300

    feat: set up environment

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+function App():JSX.Element {
+  return (
+    <div><h1 className={styles.root}>learn react</h1></div>
+  );
+}
+
+export default App;
