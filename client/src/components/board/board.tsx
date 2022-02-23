d4550a5837d1e8b8b882c6e50d9f2af13f6f6772
Dasha1801 <6227968@gmail.com>
2022-02-23 17:57:28 +0300

    feat: add modal and form to add new task

--- client/src/components/board/board.tsx
+++ client/src/components/board/board.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-import { onDragEnd } from '../../utils';
-import { columnsBoard } from '../../shared/mocks';
+import { CgAdd } from 'react-icons/cg';
 import { IColumn } from '../../shared/interfaces';
+import { columnsBoard } from '../../shared/mocks';
+import { onDragEnd } from '../../utils';
+import ModalCreateToDo from '../modal/modalCreateTodo';
 import './board.scss';
 
 function Board(): JSX.Element {
   const [columns, setColumns] = useState(columnsBoard);
+  const [show, setShow] = useState(false);
 
   const renderListItem = (column: IColumn): JSX.Element[] =>
     column.items.map((item, idx) => (
@@ -29,31 +32,37 @@ function Board(): JSX.Element {
     ));
 
   return (
-    <div className="wrapperBoard">
-      <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
-        {Object.entries(columns).map(([columnId, column], index) => (
-          <div className="itemsColumn" key={columnId}>
-            <div className="wrapperColumn">
-              <Droppable droppableId={columnId} key={columnId}>
-                {(provided, snapshot) => (
-                  <div
-                    className="column"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver ? '#dae0e6' : '#67bdff55',
-                    }}
-                  >
-                    <h5 className="nameColumn">{column.name}</h5>
-                    {renderListItem(column)}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
+    <div className="board">
+      <h5 className="titleToDo">
+        Create your ToDo <CgAdd className="iconAddToDo" onClick={() => setShow(!show)} />
+      </h5>
+      <div className="wrapperColumns">
+        <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+          {Object.entries(columns).map(([columnId, column], index) => (
+            <div className="itemsColumn" key={columnId}>
+              <div className="wrapperColumn">
+                <Droppable droppableId={columnId} key={columnId}>
+                  {(provided, snapshot) => (
+                    <div
+                      className="column"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver ? '#dae0e6' : '#67bdff55',
+                      }}
+                    >
+                      <h5 className="nameColumn">{column.name}</h5>
+                      {renderListItem(column)}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
             </div>
-          </div>
-        ))}
-      </DragDropContext>
+          ))}
+        </DragDropContext>
+        <ModalCreateToDo show={show} setState={setShow} />
+      </div>
     </div>
   );
 }
